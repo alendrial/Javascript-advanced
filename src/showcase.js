2023ec75b866c7785a62107221eceb467409a7ca
Alendrial <mashkinmv94@gmail.com>
2022-01-21 21:15:54 +0700

    1

--- src/showcase.js
+++ src/showcase.js
@@ -1,7 +1,11 @@
 const $showcase = document.querySelector('.showcase');
 
 const renderGoodsItem = ({ title, price }) => {
-  return `<div class="goods-item"><img src="https://placeimg.com/100/100/arch?t=random" alt="picture"><h3>${title}</h3><p>${price}</p></div>`;
+  return `<div class="goods-item">
+          <img src="https://placeimg.com/100/100/arch?t=${random(0, 1000)}" alt="picture">
+          <h3>${title}</h3>
+          <p>${price}</p>
+          </div>`;
 };
 
 const renderGoodsList = (list) => {
