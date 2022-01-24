5bae0c26e231d75ca556f5a59bb63a60e6c3508c
Alendrial <mashkinmv94@gmail.com>
2022-01-24 18:59:04 +0700

    Lesson 2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import renderGoodsList from "./showcase.js";
 import _ from "lodash";
 import '../public/css/style.scss';
 
-const productList = getProductList(20);
+//const productList = getProductList(20);
 
-renderGoodsList(productList);
+//renderGoodsList(productList);
 
 
+const API_URL = 'http://localhost:3000/api/v1'
+
+let productList = [];
+let cart = [];
+
+send((error) => { console.log(err) }, (res) => {
+    let list = JSON.parse(res);
+    productList = list;
+    renderGoodsList(productList);
+}, `${API_URL}/catalog`)
+
+let buyed = {id: 5, title: "new", price: 999};
+send((error) => { console.log(err) }, (res) => {
+  cart.push(buyed)
+}, `${API_URL}/cart`, 'POST', JSON.stringify(buyed), {"Content-Type": "application/json"})
