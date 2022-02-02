c3e06d76fe8dd79ddc3c3022b0b79db9738e9d68
Alendrial <mashkinmv94@gmail.com>
2022-02-02 18:57:13 +0700

    1

--- src/ApiHandler.js
+++ src/ApiHandler.js
@@ -1,4 +1,3 @@
-import {send} from './utils.js'
 export default class ApiHandler {
   constructor(apiUrl) {
     this.apiUrl = apiUrl
@@ -19,4 +18,41 @@ export default class ApiHandler {
   removeFromCart(onSuccess, onError, data) {
     this.send(onError, onSuccess, `${this.apiUrl}/cart`, 'DELETE', JSON.stringify(data), {"Content-Type": "application/json"})
   }
+
+
+  send(onError, onSuccess, url, method = 'GET', data = '', headers = {}, timeout = 60000) {
+ 
+    let xhr;
+
+    if (window.XMLHttpRequest) {
+      // Chrome, Mozilla, Opera, Safari
+      xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { 
+      // Internet Explorer
+      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhr.timeout = timeout; 
+
+    xhr.ontimeout = onError;
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if(xhr.status < 400) {
+          onSuccess(xhr.responseText)
+        } else if (xhr.status >= 400) {
+          onError(xhr.status)
+        }
+      }
+    }
+
+    xhr.open(method, url, true);
+
+    for(const [key, value] of Object.entries(headers)) {
+      xhr.setRequestHeader(key, value)
+    }
+
+    xhr.send(data);
+  }
+
 }
