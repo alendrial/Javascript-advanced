f866868d0116941796044f81f2c0eeca949c63ce
Alendrial <mashkinmv94@gmail.com>
2022-02-04 14:49:23 +0700

    lesson-4 before BACKUP

--- src/ApiHandler.js
+++ src/ApiHandler.js
@@ -1,6 +1,4 @@
-import res from 'express/lib/response'
-import { result } from 'lodash'
-import { send, sendPromise } from './utils'
+import {send, senPromise} from './utils';
 
 export default class ApiHandler {
   constructor(apiUrl) {
@@ -10,45 +8,20 @@ export default class ApiHandler {
   getCatalog(onSuccess, onError) {
     send(onError, onSuccess, `${this.apiUrl}/catalog`)
   }
-  /*
-  getCart(onSuccess, onError) {
-    send(onError, onSuccess, `${this.apiUrl}/cart`)
-  }
-  */
+
   getCart(onSuccess, onError) {
     return sendPromise(`${this.apiUrl}/cart`)
-    .then((data) => {
-      return JSON.stringify(data)
-    })
+      .then((data) => {
+        return JSON.stringify(data)
+      })
   }
-  
 
   addToCart(onSuccess, onError, data) {
     send(onError, onSuccess, `${this.apiUrl}/cart`, 'POST', JSON.stringify(data), {"Content-Type": "application/json"})
   }
-    /*
-    addToCart(apiUrl, data) {
-      return fetch(`${this.apiUrl}`, {
-        method: 'PUT', 
-        headers: {
-          "Content-Type": "application/json"
-         },
-         body: JSON.stringify(data)
-        }).then(result => result.json())
-    }
-    */
+
   removeFromCart(onSuccess, onError, data) {
     send(onError, onSuccess, `${this.apiUrl}/cart`, 'DELETE', JSON.stringify(data), {"Content-Type": "application/json"})
   }
-  /*
-     removeFromCart(apiUrl) {
-       return fetch(`${this.apiUrl}`,{
-         method: 'DELETE',
-         headers: {
-          "Content-Type": "application/json"
-         },
-       }).then(result => result.json())
-     }
-     */
 
 }
