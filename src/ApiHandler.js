c048d7c4255b92a2818b99611acc8bc37a1124d2
Alendrial <mashkinmv94@gmail.com>
2022-02-04 14:56:47 +0700

    s

--- src/ApiHandler.js
+++ src/ApiHandler.js
@@ -9,13 +9,18 @@ export default class ApiHandler {
     send(onError, onSuccess, `${this.apiUrl}/catalog`)
   }
 
+  
+  getCart(onSuccess, onError) {
+    send(onError, onSuccess, `${this.apiUrl}/catalog`)
+  }
+  /*
   getCart(onSuccess, onError) {
     return sendPromise(`${this.apiUrl}/cart`)
       .then((data) => {
         return JSON.stringify(data)
       })
   }
-
+  */
   addToCart(onSuccess, onError, data) {
     send(onError, onSuccess, `${this.apiUrl}/cart`, 'POST', JSON.stringify(data), {"Content-Type": "application/json"})
   }
